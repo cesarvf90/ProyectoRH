1e9e534413314e2512e3c9d880dd89d117561a35
César Vásquez Flores <cesarvf90@gmail.com>
2013-04-21 23:40:31 -0500

    Se sube la pantalla de login integrada

--- gen/pe/edu/pucp/proyectorh/R.java
+++ gen/pe/edu/pucp/proyectorh/R.java
@@ -11,23 +11,40 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int backgreen=0x7f020000;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int accordion_list=0x7f060000;
-        public static final int opcion_detail=0x7f060003;
-        public static final int opcion_detail_container=0x7f060001;
-        public static final int opcion_list=0x7f060002;
+        public static final int accordion_list=0x7f070000;
+        public static final int button1=0x7f070006;
+        public static final int editText1=0x7f070003;
+        public static final int editText2=0x7f070005;
+        public static final int linearLayout1=0x7f070001;
+        public static final int menu_settings=0x7f07000a;
+        public static final int opcion_detail=0x7f070009;
+        public static final int opcion_detail_container=0x7f070007;
+        public static final int opcion_list=0x7f070008;
+        public static final int textView1=0x7f070002;
+        public static final int textView2=0x7f070004;
     }
     public static final class layout {
         public static final int accordion_list=0x7f030000;
-        public static final int activity_opcion_detail=0x7f030001;
-        public static final int activity_opcion_list=0x7f030002;
-        public static final int activity_opcion_twopane=0x7f030003;
-        public static final int fragment_opcion_detail=0x7f030004;
+        public static final int activity_login=0x7f030001;
+        public static final int activity_opcion_detail=0x7f030002;
+        public static final int activity_opcion_list=0x7f030003;
+        public static final int activity_opcion_twopane=0x7f030004;
+        public static final int fragment_opcion_detail=0x7f030005;
+    }
+    public static final class menu {
+        public static final int activity_login=0x7f060000;
     }
     public static final class string {
+        public static final int Login=0x7f040006;
         public static final int app_name=0x7f040000;
+        public static final int hello_world=0x7f040003;
+        public static final int menu_settings=0x7f040004;
+        public static final int title_activity_login=0x7f040005;
         public static final int title_opcion_detail=0x7f040001;
         public static final int title_opcion_list=0x7f040002;
     }
---
+++ src/pe/edu/pucp/proyectorh/LoginActivity.java
@@ -0,0 +1,51 @@
+package pe.edu.pucp.proyectorh;
+
+import pe.edu.pucp.proyectorh.utils.Constante;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+		/* Cambiar color de la vista */
+		// View view = findViewById(R.id.linearLayout1);
+		// View root = view.getRootView();
+		// root.setBackgroundColor(getResources().getColor(
+		// android.R.color.holo_green_light));
+		Button loginButton = (Button) findViewById(R.id.button1);
+		loginButton.getBackground().setColorFilter(0xFF91E2A9,
+				PorterDuff.Mode.MULTIPLY);
+		loginButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				EditText usuarioText = (EditText) findViewById(R.id.editText1);
+				EditText contrasenaText = (EditText) findViewById(R.id.editText2);
+				if (!Constante.CADENA_VACIA.equals(usuarioText.getText()
+						.toString())
+						&& !Constante.CADENA_VACIA.equals(contrasenaText
+								.getText().toString())) {
+					Button loginButton = (Button) findViewById(R.id.button1);
+					Intent mainIntent = new Intent(v.getContext(),
+							OpcionListActivity.class);
+					startActivity(mainIntent);
+				}
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_login, menu);
+		return true;
+	}
+}
