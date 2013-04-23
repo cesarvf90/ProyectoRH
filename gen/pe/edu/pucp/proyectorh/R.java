b7621e671fa7dd07a2d8829f4e37185f249fc72e
Ever Mitta <emitta@pucp.pe>
2013-04-23 15:10:25 -0500

    git add <file>...

--- gen/pe/edu/pucp/proyectorh/R.java
+++ gen/pe/edu/pucp/proyectorh/R.java
@@ -8,6 +8,9 @@
 package pe.edu.pucp.proyectorh;
 
 public final class R {
+    public static final class array {
+        public static final int periodos=0x7f040000;
+    }
     public static final class attr {
     }
     public static final class drawable {
@@ -16,19 +19,24 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int about=0x7f07000b;
-        public static final int accordion_list=0x7f070000;
-        public static final int contrasenaText=0x7f070005;
-        public static final int help=0x7f07000c;
-        public static final int linearLayout1=0x7f070001;
-        public static final int loginButton=0x7f070006;
-        public static final int menu_settings=0x7f07000a;
-        public static final int opcion_detail=0x7f070009;
-        public static final int opcion_detail_container=0x7f070007;
-        public static final int opcion_list=0x7f070008;
-        public static final int textView1=0x7f070002;
-        public static final int textView2=0x7f070004;
-        public static final int usuarioText=0x7f070003;
+        public static final int about=0x7f080010;
+        public static final int accordion_list=0x7f080000;
+        public static final int contrasenaText=0x7f080005;
+        public static final int help=0x7f080011;
+        public static final int linearLayout1=0x7f080001;
+        public static final int loginButton=0x7f080006;
+        public static final int menu_settings=0x7f08000f;
+        public static final int opcion_detail=0x7f080009;
+        public static final int opcion_detail_container=0x7f080007;
+        public static final int opcion_list=0x7f080008;
+        public static final int spinner1=0x7f08000a;
+        public static final int tab1=0x7f08000b;
+        public static final int tab2=0x7f08000c;
+        public static final int tab3=0x7f08000d;
+        public static final int tab4=0x7f08000e;
+        public static final int textView1=0x7f080002;
+        public static final int textView2=0x7f080004;
+        public static final int usuarioText=0x7f080003;
     }
     public static final class layout {
         public static final int accordion_list=0x7f030000;
@@ -37,21 +45,22 @@ public final class R {
         public static final int activity_opcion_list=0x7f030003;
         public static final int activity_opcion_twopane=0x7f030004;
         public static final int fragment_opcion_detail=0x7f030005;
+        public static final int objetivosbsc=0x7f030006;
     }
     public static final class menu {
-        public static final int activity_login=0x7f060000;
-        public static final int options_menu=0x7f060001;
+        public static final int activity_login=0x7f070000;
+        public static final int options_menu=0x7f070001;
     }
     public static final class string {
-        public static final int Login=0x7f040006;
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040003;
-        public static final int menu_settings=0x7f040004;
-        public static final int title_activity_login=0x7f040005;
-        public static final int title_opcion_detail=0x7f040001;
-        public static final int title_opcion_list=0x7f040002;
+        public static final int Login=0x7f050006;
+        public static final int app_name=0x7f050000;
+        public static final int hello_world=0x7f050003;
+        public static final int menu_settings=0x7f050004;
+        public static final int title_activity_login=0x7f050005;
+        public static final int title_opcion_detail=0x7f050001;
+        public static final int title_opcion_list=0x7f050002;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f060000;
     }
 }
--- src/pe/edu/pucp/proyectorh/OpcionDetailFragment.java
+++ src/pe/edu/pucp/proyectorh/OpcionDetailFragment.java
@@ -2,11 +2,18 @@ package pe.edu.pucp.proyectorh;
 
 import pe.edu.pucp.proyectorh.model.Modulo;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TabHost;
+import android.widget.TabHost.OnTabChangeListener;
 import android.widget.TextView;
 
 /**
@@ -36,14 +43,75 @@ public class OpcionDetailFragment extends Fragment {
 		}
 	}
 
+	@SuppressWarnings("rawtypes")
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.fragment_opcion_detail,
-				container, false);
+			View rootView;
 		if (tituloFuncionalidad != null) {
-			((TextView) rootView.findViewById(R.id.opcion_detail))
-					.setText(tituloFuncionalidad.nombre);
+			if(tituloFuncionalidad.nombre.equals("Objetivos Empresa")){
+				rootView = inflater.inflate(R.layout.objetivosbsc,
+						container, false);
+				rootView.findViewById(R.layout.objetivosbsc);
+				String array_spinner[];
+				array_spinner=new String[5];
+				array_spinner[0]="01/01/2013 al 31/12/2013";
+				array_spinner[1]="01/01/2012 al 31/12/2012";
+				array_spinner[2]="01/01/2011 al 31/12/2011";
+				array_spinner[3]="01/01/2010 al 31/12/2010";
+				array_spinner[4]="01/01/2009 al 31/12/2009";
+				Spinner s = (Spinner) rootView.findViewById(R.id.spinner1);
+				@SuppressWarnings("unchecked")
+				ArrayAdapter adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_spinner_item, array_spinner);
+				s.setAdapter(adapter);
+				
+				Resources res = getResources();
+				 
+				TabHost tabs=(TabHost)rootView.findViewById(android.R.id.tabhost);
+				tabs.setup();
+				 
+				TabHost.TabSpec spec=tabs.newTabSpec("Cliente");
+				spec.setContent(R.id.tab1);
+				spec.setIndicator("Cliente",
+				    res.getDrawable(android.R.drawable.ic_btn_speak_now));
+				tabs.addTab(spec);
+				 
+				spec=tabs.newTabSpec("Aprendizaje y Crecimiento");
+				spec.setContent(R.id.tab2);
+				spec.setIndicator("Aprendizaje y Crecimiento",
+				    res.getDrawable(android.R.drawable.ic_dialog_map));
+				tabs.addTab(spec);
+				
+				spec=tabs.newTabSpec("Financiero");
+				spec.setContent(R.id.tab3);
+				spec.setIndicator("Financiero",
+				    res.getDrawable(android.R.drawable.ic_dialog_map));
+				tabs.addTab(spec);
+				
+				spec=tabs.newTabSpec("Procesos Internos");
+				spec.setContent(R.id.tab2);
+				spec.setIndicator("Procesos Internos",
+				    res.getDrawable(android.R.drawable.ic_dialog_map));
+				tabs.addTab(spec);
+				 
+				tabs.setCurrentTab(0);
+				
+				tabs.setOnTabChangedListener(new OnTabChangeListener(){
+				    @Override
+				    public void onTabChanged(String tabId) {
+				    	
+				        Log.i("AndroidTabsDemo", "Pulsada pestaña: " + tabId);
+				    }
+				});
+			}else{
+				rootView = inflater.inflate(R.layout.fragment_opcion_detail,
+						container, false);
+				((TextView) rootView.findViewById(R.id.opcion_detail))
+				.setText(tituloFuncionalidad.nombre);
+			}
+		}else{
+			rootView = inflater.inflate(R.layout.fragment_opcion_detail,
+					container, false);
 		}
 		return rootView;
 	}
