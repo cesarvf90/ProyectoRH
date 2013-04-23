104cb398c660b0fdba40694c96489cba0ca45e99
Ever Mitta <emitta@pucp.pe>
2013-04-23 15:15:33 -0500

    ever

--- gen/pe/edu/pucp/proyectorh/R.java
+++ gen/pe/edu/pucp/proyectorh/R.java
@@ -19,17 +19,23 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-<<<<<<< HEAD
-        public static final int about=0x7f080010;
+        public static final int about=0x7f080018;
         public static final int accordion_list=0x7f080000;
         public static final int contrasenaText=0x7f080005;
-        public static final int help=0x7f080011;
+        public static final int finalizar=0x7f080016;
+        public static final int help=0x7f080019;
         public static final int linearLayout1=0x7f080001;
         public static final int loginButton=0x7f080006;
-        public static final int menu_settings=0x7f08000f;
+        public static final int menu_settings=0x7f080017;
         public static final int opcion_detail=0x7f080009;
         public static final int opcion_detail_container=0x7f080007;
         public static final int opcion_list=0x7f080008;
+        public static final int pregunta1=0x7f080010;
+        public static final int pregunta2=0x7f080012;
+        public static final int pregunta3=0x7f080014;
+        public static final int ratingPregunta1=0x7f080011;
+        public static final int ratingPregunta2=0x7f080013;
+        public static final int ratingPregunta3=0x7f080015;
         public static final int spinner1=0x7f08000a;
         public static final int tab1=0x7f08000b;
         public static final int tab2=0x7f08000c;
@@ -37,30 +43,8 @@ public final class R {
         public static final int tab4=0x7f08000e;
         public static final int textView1=0x7f080002;
         public static final int textView2=0x7f080004;
+        public static final int title=0x7f08000f;
         public static final int usuarioText=0x7f080003;
-=======
-        public static final int about=0x7f070013;
-        public static final int accordion_list=0x7f070000;
-        public static final int contrasenaText=0x7f070005;
-        public static final int finalizar=0x7f070011;
-        public static final int help=0x7f070014;
-        public static final int linearLayout1=0x7f070001;
-        public static final int loginButton=0x7f070006;
-        public static final int menu_settings=0x7f070012;
-        public static final int opcion_detail=0x7f070009;
-        public static final int opcion_detail_container=0x7f070007;
-        public static final int opcion_list=0x7f070008;
-        public static final int pregunta1=0x7f07000b;
-        public static final int pregunta2=0x7f07000d;
-        public static final int pregunta3=0x7f07000f;
-        public static final int ratingPregunta1=0x7f07000c;
-        public static final int ratingPregunta2=0x7f07000e;
-        public static final int ratingPregunta3=0x7f070010;
-        public static final int textView1=0x7f070002;
-        public static final int textView2=0x7f070004;
-        public static final int title=0x7f07000a;
-        public static final int usuarioText=0x7f070003;
->>>>>>> 285887ad956a1c1f89a83a23cda4f65986a36937
     }
     public static final class layout {
         public static final int accordion_list=0x7f030000;
@@ -69,11 +53,8 @@ public final class R {
         public static final int activity_opcion_list=0x7f030003;
         public static final int activity_opcion_twopane=0x7f030004;
         public static final int fragment_opcion_detail=0x7f030005;
-<<<<<<< HEAD
         public static final int objetivosbsc=0x7f030006;
-=======
-        public static final int rendir_evaluaciones=0x7f030006;
->>>>>>> 285887ad956a1c1f89a83a23cda4f65986a36937
+        public static final int rendir_evaluaciones=0x7f030007;
     }
     public static final class menu {
         public static final int activity_login=0x7f070000;
