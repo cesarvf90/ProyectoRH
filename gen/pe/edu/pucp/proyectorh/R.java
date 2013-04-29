2ce79de36b4d05efc825f19bc044fd9306d43c16
César Vásquez Flores <cesarvf90@gmail.com>
2013-04-28 21:09:21 -0500

    Se customiza el menu izquierdo

--- gen/pe/edu/pucp/proyectorh/R.java
+++ gen/pe/edu/pucp/proyectorh/R.java
@@ -20,6 +20,8 @@ public final class R {
         public static final int ic_launcher=0x7f020003;
         public static final int logorh1=0x7f020004;
         public static final int logorh2=0x7f020005;
+        public static final int textview_back=0x7f020006;
+        public static final int textview_backred=0x7f020007;
     }
     public static final class id {
         public static final int about=0x7f08001b;
@@ -71,6 +73,7 @@ public final class R {
         public static final int Login=0x7f050006;
         public static final int Mensaje_Bienvenida=0x7f050009;
         public static final int app_name=0x7f050000;
+        public static final int evaluar_postulante=0x7f05000a;
         public static final int hello_world=0x7f050003;
         public static final int menu_settings=0x7f050004;
         public static final int password=0x7f050007;
