1cac8abae766338fa4627ecbd72d7a7c9670eb14
César Vásquez Flores <cesarvf90@gmail.com>
2013-05-05 23:10:34 -0500

    Se crean clases del modelo

---
+++ src/pe/edu/pucp/proyectorh/model/Colaborador.java
@@ -0,0 +1,129 @@
+package pe.edu.pucp.proyectorh.model;
+
+import pe.edu.pucp.proyectorh.utils.Constante;
+
+/**
+ * Clase Colaborador mapeada contra ColaboradorDTO de la aplicacion web
+ * 
+ * @author Cesar
+ * 
+ */
+public class Colaborador {
+
+	private String nombres;
+	private String apellidos;
+	private String area;
+	private String puesto;
+	private String email;
+	private String anexo;
+	private String fechaIngreso;
+	private String fechaNacimiento;
+	private String correoElectronico;
+	private String telefono;
+	private String cenrtoEstudios;
+
+	public String getCenrtoEstudios() {
+		return cenrtoEstudios;
+	}
+
+	public void setCenrtoEstudios(String cenrtoEstudios) {
+		this.cenrtoEstudios = cenrtoEstudios;
+	}
+
+	public String getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(String fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public String getCorreoElectronico() {
+		return correoElectronico;
+	}
+
+	public void setCorreoElectronico(String correoElectronico) {
+		this.correoElectronico = correoElectronico;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public Colaborador() {
+	}
+
+	public Colaborador(String nombres, String apellidos, String area,
+			String puesto) {
+		super();
+		this.nombres = nombres;
+		this.apellidos = apellidos;
+		this.area = area;
+		this.puesto = puesto;
+	}
+
+	public String getNombres() {
+		return nombres;
+	}
+
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getArea() {
+		return area;
+	}
+
+	public void setArea(String area) {
+		this.area = area;
+	}
+
+	public String getPuesto() {
+		return puesto;
+	}
+
+	public void setPuesto(String puesto) {
+		this.puesto = puesto;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAnexo() {
+		return anexo;
+	}
+
+	public void setAnexo(String anexo) {
+		this.anexo = anexo;
+	}
+
+	public String getFechaIngreso() {
+		return fechaIngreso;
+	}
+
+	public void setFechaIngreso(String fechaIngreso) {
+		this.fechaIngreso = fechaIngreso;
+	}
+
+	@Override
+	public String toString() {
+		return nombres + Constante.ESPACIO_VACIO + apellidos;
+	}
+}
