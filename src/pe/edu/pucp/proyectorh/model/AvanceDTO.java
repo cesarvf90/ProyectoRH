d8ebbda13533f4a31283acdc818318fc1f39e152
Ever Mitta Flores <emitta@pucp.pe>
2013-06-11 20:29:14 -0500

    eve

---
+++ src/pe/edu/pucp/proyectorh/model/AvanceDTO.java
@@ -0,0 +1,13 @@
+package pe.edu.pucp.proyectorh.model;
+
+public class AvanceDTO {
+	public int ID;
+	public int Valor;
+	public String FechaCreacion;
+	public int CreadorID;
+	public int ObjetivoID;
+	public boolean FueRevisado;
+	public int ValorDelJefe;
+	public String Comentario;
+
+}
