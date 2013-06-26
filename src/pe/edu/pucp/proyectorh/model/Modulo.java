02f1cfaaf532ddf9c78aad401ca0c5819b68047f
André Montoya <andre.montoyadp@gmail.com>
2013-06-26 14:09:45 -0500

    nombre de reporte

--- src/pe/edu/pucp/proyectorh/model/Modulo.java
+++ src/pe/edu/pucp/proyectorh/model/Modulo.java
@@ -114,7 +114,7 @@ public class Modulo {
 		submodulos.add(new ModuloItem("1", "Reporte de Evaluación 360"));
 		submodulos
 				.add(new ModuloItem("2", "Reporte histórico de Objetivos"));
-		submodulos.add(new ModuloItem("3", "Reporte de Cubrimiento de Puestos"));
+		submodulos.add(new ModuloItem("3", "Reporte de Ofertas Laborales"));
 		submodulos.add(new ModuloItem("4", "Reporte de Objetivos BSC"));
 		return submodulos;
 	}
